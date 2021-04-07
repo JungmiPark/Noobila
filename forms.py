9a26bf1d78d2ddb16bf58d92d55fa3931e265f6b
JungmiPark <53827648+JungmiPark@users.noreply.github.com>
2021-04-07 23:32:33 +0900

    지도에 터미널 위치 표시

--- forms.py
+++ forms.py
@@ -3,21 +3,22 @@ import os
 import sys
 import json
 
-url = 'http://api.nubija.com:1577/ubike/nubijaInfoApi.do?apikey=lkZqiljssOIElXPpnwXd'
-request = urllib.request.Request(url)
-response = urllib.request.urlopen(request)
 
-if response.getcode() == 200:
-    #통신 성공시 처리 -> postman으로 통신 수행 및 응답결과 확인(구조확인)
-    #결과는 json -> json 처리하는 모듈 필요
-    tmp = json.load(response)
-    print(tmp)
-    pass
-else:
-    #통신 실패시 처리
-    print('error code', response.getcode())
+def get_terminalinfo():
+    key = 'lkZqiljssOIElXPpnwXd'
+    url = f'http://api.nubija.com:1577/ubike/nubijaInfoApi.do?apikey={key}'
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
 
-terminalInfo = tmp['TerminalInfo']
+    if response.getcode() == 200:
+        #통신 성공시 처리 -> postman으로 통신 수행 및 응답결과 확인(구조확인)
+        #결과는 json -> json 처리하는 모듈 필요
+        info = json.load(response)
+        print(info)
+        pass
+    else:
+        #통신 실패시 처리
+        print('error code', response.getcode())
+        return None
 
-for i in terminalInfo:
-  print(i['Vno'])
+    return info['TerminalInfo']
--- views/main_views.py
+++ views/main_views.py
@@ -1,11 +1,37 @@
 from flask import Blueprint, render_template
+import urllib
+import os
+import sys
+import json
+# from ..forms import get_terminalinfo
 
 bp = Blueprint('main', __name__, url_prefix='/')
+
+def get_terminalinfo():
+    key = 'lkZqiljssOIElXPpnwXd'
+    url = f'http://api.nubija.com:1577/ubike/nubijaInfoApi.do?apikey={key}'
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+
+    if response.getcode() == 200:
+        #통신 성공시 처리 -> postman으로 통신 수행 및 응답결과 확인(구조확인)
+        #결과는 json -> json 처리하는 모듈 필요
+        info = json.load(response)
+        print(info['Regdate'])
+        pass
+    else:
+        #통신 실패시 처리
+        print('error code', response.getcode())
+        return None
+
+    return info['TerminalInfo']
+
 
 @bp.route('/')
 def hi_nubila():
     return 'Hi! nubila'
 
-@bp.route('/home')
+@bp.route('/home', methods=['GET', 'POST'])
 def nubila():
-    return render_template('home.html')
+    terminalinfo = get_terminalinfo()
+    return render_template('home.html', terminalinfo = terminalinfo)
